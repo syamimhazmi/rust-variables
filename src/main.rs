a29c405bfb368668cd5ca18e47c01fa22fef5359
syamimhazmi <syamimhazmi@gmail.com>
2024-01-11 00:02:09 +0700

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
 
-    println!("The value of x is: {x}");
+    let x = x + 1;
 
-    x = 6;
+    {
+        let x = x * 2;
+        
+        println!("The value of x in the inner scope is: {x}");
+    }
 
     println!("The value of x is: {x}");
 }
